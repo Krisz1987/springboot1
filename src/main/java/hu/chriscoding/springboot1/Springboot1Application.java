e2c2ac6f3de5d2beeb4f7ad59502e02cc39f1f9a
Krisz1987 <krisztian55@gmail.com>
2021-10-08 19:13:58 +0200

    10.08 első változtatás

--- src/main/java/hu/chriscoding/springboot1/Springboot1Application.java
+++ src/main/java/hu/chriscoding/springboot1/Springboot1Application.java
@@ -2,12 +2,19 @@ package hu.chriscoding.springboot1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Springboot1Application {
 
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(Springboot1Application.class, args);
+		
 	}
 
 }
---
+++ src/main/java/hu/chriscoding/springboot1/controller/HomeController.java
@@ -0,0 +1,16 @@
+package hu.chriscoding.springboot1.controller;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
